2332e73bd5926ad0155820fff488000a0c324c0d
stacks-of-data <coder64bit@gmail.com>
2026-05-30 23:02:19 +0300

    refactor: MaterialDescriptor now takes vert, frag entry names

--- src/resource/descriptors.rs
+++ src/resource/descriptors.rs
@@ -26,7 +26,39 @@ pub struct TextureDescriptor {
 #[derive(Debug, Clone)]
 pub struct MaterialDescriptor {
     pub vertex_shader_spv: Vec<u8>,
+    pub vertex_entry: String,
     pub fragment_shader_spv: Vec<u8>,
+    pub fragment_entry: String,
     pub enable_depth: bool,
     pub texture: Option<TextureId>,
+}
+
+impl MaterialDescriptor {
+    pub fn new(
+        vertex_shader_spv: Vec<u8>,
+        vertex_entry: String,
+        fragment_shader_spv: Vec<u8>,
+        fragment_entry: String,
+        enable_depth: bool,
+        texture: Option<TextureId>,
+    ) -> Self {
+        Self {
+            vertex_shader_spv,
+            vertex_entry,
+            fragment_shader_spv,
+            fragment_entry,
+            enable_depth,
+            texture,
+        }
+    }
+
+    pub fn with_vertex_entry(mut self, entry: impl Into<String>) -> Self {
+        self.vertex_entry = entry.into();
+        self
+    }
+
+    pub fn with_fragment_entry(mut self, entry: impl Into<String>) -> Self {
+        self.fragment_entry = entry.into();
+        self
+    }
 }
